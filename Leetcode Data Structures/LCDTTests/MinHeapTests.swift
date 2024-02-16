f80fb002087236c7d62daabf7aa3724ddf58d491
Yevhen Dubinin <thisiskilling@me.com>
2024-02-16 11:11:08 -0800

    Test build heap with elements

--- Leetcode Data Structures/LCDTTests/MinHeapTests.swift
+++ Leetcode Data Structures/LCDTTests/MinHeapTests.swift
@@ -11,7 +11,38 @@ import LCDT
 class MinHeapTests: XCTestCase {
 
     func test_init_heapIsEmptyOnInit() {
-        let sut = MinHeap<Int>()
+        let sut = makeSUT()
         XCTAssertEqual(sut.count, 0)
+    }
+
+    func test_build_emptyWhenBuiltWithEmptyArray() {
+        var sut = makeSUT()
+
+        sut.build(from: [])
+
+        XCTAssertEqual(sut.count, 0)
+    }
+
+    func test_build_hasCountOfNumberOfElements() {
+        var sut = makeSUT()
+
+        let dataSet: [[Int]] = [
+            [1],
+            [1,2],
+            [1,2,3]
+        ]
+
+        for ds in dataSet {
+            let expectedCount = ds.count
+            sut.build(from: ds)
+            XCTAssertEqual(sut.count, expectedCount)
+        }
+    }
+
+    // MARK: Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> MinHeap<Int> {
+        let sut = MinHeap<Int>()
+        return sut
     }
 }
